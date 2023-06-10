9a6184abc18a5ba67d719736c278d77789022592
Andrii Khoma <anik123wb@gmail.com>
2023-06-10 16:37:13 +0300

    use money big and small

--- formatPrice.ts
+++ formatPrice.ts
@@ -1,16 +1,23 @@
-import {CURRENCY, CURRENCY_TO_SYMBOL} from "../shop-shared/constants/exchange";
+import {
+  CURRENCY,
+  CURRENCY_TO_SYMBOL,
+} from '../shop-shared/constants/exchange';
+import { MoneyBig } from '../shop-shared/dto/primitiveTypes';
 
-export function formatPrice(price: number, currency: CURRENCY): string {
-    let priceString = price.toFixed(2);
-    if (priceString.endsWith('.00')) {
-        priceString = priceString.slice(0, -3);
-    }
+export function formatPrice(
+  price: number | MoneyBig,
+  currency: CURRENCY,
+): string {
+  let priceString = (price as number).toFixed(2);
+  if (priceString.endsWith('.00')) {
+    priceString = priceString.slice(0, -3);
+  }
 
-    const symbol = CURRENCY_TO_SYMBOL[currency];
+  const symbol = CURRENCY_TO_SYMBOL[currency];
 
-    if (currency === CURRENCY.UAH) {
-        return `${priceString} ${symbol}`;
-    } else {
-        return `${symbol}${priceString}`;
-    }
+  if (currency === CURRENCY.UAH) {
+    return `${priceString} ${symbol}`;
+  } else {
+    return `${symbol}${priceString}`;
+  }
 }
