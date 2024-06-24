ada7458234a7571b34a391db8954c899a3b61554
anikkhoma <anik123wb@gmail.com>
2024-06-24 16:42:37 +0300

    refactor

--- loadExchangeState.ts
+++ loadExchangeState.ts
@@ -1,32 +1,42 @@
-import { CURRENCIES, CurrencyEnum } from "../shop-shared/constants/exchange";
+import * as assert from "assert";
+
+import { CURRENCIES, CurrencyEnum } from "@/shop-shared/constants/exchange";
+
 import { createExchangeKey, ExchangeState, parseExchange } from "./helpers";
 import { redisClient } from "./redisConnection";
 import { applyStaticExchange } from "./staticStore";
-import * as assert from "assert";
 
 export async function loadExchangeState(): Promise<ExchangeState> {
 	const exchangeState: ExchangeState = {};
-
 	const currencies = CURRENCIES.filter((currency) => currency !== CurrencyEnum.UAH);
-	const pipeline = redisClient.pipeline();
-	for (const currency of currencies) pipeline.get(createExchangeKey(currency, CurrencyEnum.UAH));
-	const response = await pipeline.exec();
-	if (!response) {
-		throw new Error(`Cannot get exchange rates from redis`);
-	}
 
-	for (const [index, currency] of currencies.entries()) {
-		const responseEntry = response[index];
-		assert.ok(responseEntry, `Cannot get exchange rate for ${currency}`);
-		const [error, value] = responseEntry;
-		if (error) {
-			throw error;
+	try {
+		const pipeline = redisClient.pipeline();
+
+		for (const currency of currencies)
+			pipeline.get(createExchangeKey(currency, CurrencyEnum.UAH));
+
+		const response = await pipeline.exec();
+		if (!response) {
+			throw new Error(`Cannot get exchange rates from redis`);
 		}
-		const key = createExchangeKey(currency, CurrencyEnum.UAH);
-		exchangeState[key] = parseExchange(value as string);
+
+		for (const [index, currency] of currencies.entries()) {
+			const responseEntry = response[index];
+			assert.ok(responseEntry, `Cannot get exchange rate for ${currency}`);
+			const [error, value] = responseEntry;
+			if (error) {
+				throw error;
+			}
+			const key = createExchangeKey(currency, CurrencyEnum.UAH);
+			exchangeState[key] = parseExchange(value as string);
+		}
+
+		applyStaticExchange(exchangeState);
+
+		return exchangeState;
+	} catch (error) {
+		console.error(`Cannot load exchange state: ${error}`);
+		return exchangeState;
 	}
-
-	applyStaticExchange(exchangeState);
-
-	return exchangeState;
 }
--- redisConnection.ts
+++ redisConnection.ts
@@ -4,6 +4,7 @@ export const redisClient = new Redis({
 	host: process.env["REDIS_HOST"],
 	port: Number.parseInt(process.env["REDIS_PORT"] || "6379"),
 });
+
 redisClient.on("error", (error: unknown) => {
-	console.error("[Redis] Error", error);
+	console.error("[Redis]", error);
 });
