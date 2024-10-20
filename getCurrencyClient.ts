44e21a9481742af97f286c027af08f561fe847d0
anikkhoma <anik123wb@gmail.com>
2024-10-20 23:46:21 +0300

    move next-cookies helpers from shared submodule

--- getCurrencyClient.ts
+++
@@ -1,7 +0,0 @@
-import { CurrencyEnum } from "../shop-shared/constants/exchange";
-import { getCookie } from "./cookieClientHelper";
-
-export function getCurrencyClient(): CurrencyEnum {
-	const currencyCookie = getCookie("currency");
-	return currencyCookie ? (currencyCookie as CurrencyEnum) : CurrencyEnum.UAH;
-}
